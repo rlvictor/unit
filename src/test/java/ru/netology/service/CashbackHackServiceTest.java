517f38252a2e5c50952e0654e60b34e72964a50d
RLVictor <vityad888@yandex.ru>
2023-08-13 17:04:49 +0300

    testint junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,44 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void shouldCashbackHackServiceBefore() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldCashbackHackServiceThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCashbackHackServiceAfter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+
 }
