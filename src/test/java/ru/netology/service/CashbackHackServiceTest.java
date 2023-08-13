295d58e4a53c8df7ad52e580008dbdf5ea8edc54
RLVictor <vityad888@yandex.ru>
2023-08-13 17:28:45 +0300

    testing test NG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,44 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void shouldCashbackHackServiceBefore() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test
+    public void shouldCashbackHackServiceThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashbackHackServiceAfter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+
 }
